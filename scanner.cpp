ae15eff5a741555e40500705f3cc2855a7af9a37
reteu5 <apeach5@kakao.com>
2023-05-14 16:34:23 +0900

    mollllu

--- scanner.cpp
+++ scanner.cpp
@@ -8,22 +8,28 @@
 #include "PEparser.h"
 #include "scanner.h"
 
+using namespace scanner;
 using std::hex;
 
 void scannerMain() {
+	//dbg
+	tcout << _T("ScannerMain() called") << std::endl;
+	
+	PEParse::PEParser peclass = PEParse::PEParser();
+
 	HANDLE PEFileMapping = NULL;
 	tstring filePath = _T("C:\\Windows\\System32\\shell32.dll");
 
 	PEFileMapping = peclass.getPEFileMapping(filePath);
 }
 
-void scanner::Scanner::debug(tstring debugmsg) {
+void Scanner::debug(tstring debugmsg) {
 	OutputDebugStringT(debugmsg.c_str());
 	OutputDebugStringT(_T("\n"));
 }
 
 //PE 파일 경로를 입력받아 .text 섹션의 전체 크기를 구하는 함수
-DWORD scanner::Scanner::getTextSectionSize(const tstring filePath) {
+DWORD Scanner::getTextSectionSize(const tstring filePath) {
 	PEParse::PEParser peclass = PEParse::PEParser();
 
 	HANDLE peFileMapping = NULL;
@@ -50,7 +56,7 @@ DWORD scanner::Scanner::getTextSectionSize(const tstring filePath) {
 }
 
 // 매개변수로 입력받은 크기만큼의 메모리를 할당하고, 할당된 메모리에 PE 파일의 .text 섹션 바이트값 전체를 복사하는 함수
-BYTE* scanner::Scanner::getTextSectionBytes(const tstring filePath, DWORD sectionSize) {
+BYTE* Scanner::getTextSectionBytes(const tstring filePath, DWORD sectionSize) {
 	PEParse::PEParser peclass = PEParse::PEParser(); 
 	HANDLE peFileMapping = NULL;
 	LPVOID peBaseAddress = NULL;
@@ -78,12 +84,12 @@ BYTE* scanner::Scanner::getTextSectionBytes(const tstring filePath, DWORD sectio
 }
 
 //getTextSectionBytes 함수에서 할당한 메모리를 해제하는 함수
-void scanner::freeTextSectionBytes(BYTE* sectionBytes) {
+void Scanner::freeTextSectionBytes(BYTE* sectionBytes) {
 	delete[] sectionBytes;
 }
 
 //getTextSectionBytes 함수에서 할당한 메모리에 대해 악성코드 패턴을 검사하는 함수
-BOOL scanner::scanMalwarePattern(BYTE* sectionBytes, DWORD sectionSize) {
+BOOL Scanner::scanMalwarePattern(BYTE* sectionBytes, DWORD sectionSize) {
 	// 악성코드 패턴을 저장할 배열
 	BYTE malwarePattern[8] = { 0x55, 0x8B, 0xEC, 0x83, 0xEC, 0x10, 0x53, 0x56 };
 	// 악성코드 패턴의 크기
@@ -105,8 +111,8 @@ BOOL scanner::scanMalwarePattern(BYTE* sectionBytes, DWORD sectionSize) {
 	return FALSE;
 }
 
-//getTextSectionBytes 함수에서 읽어온 바이트 갑 전체를 출력하는 디버깅용 함수
-void scanner::debugTextSectionBytes(BYTE* sectionBytes, DWORD sectionSize) {
+// getTextSectionBytes 함수에서 읽어온 바이트 갑 전체를 출력하는 디버깅용 함수
+void Scanner::debugTextSectionBytes(BYTE* sectionBytes, DWORD sectionSize) {
 	for (int i = 0; i < sectionSize; i++) {
 		tcout << hex << (int)sectionBytes[i] << _T(" ");
 		if (i % 16 == 15) 
@@ -114,16 +120,24 @@ void scanner::debugTextSectionBytes(BYTE* sectionBytes, DWORD sectionSize) {
 	}
 }
 
-//EntryPointSection 정보를 이용해서 처음 실행되는 코드가 포함된 섹션을 찾는 함수
-DWORD scanner::Scanner::getEntryPointSection(const tstring filePath) {
+// EntryPointSection 정보를 이용해서 처음 실행되는 코드가 포함된 섹션을 찾는 함수
+DWORD Scanner::getEntryPointSection(const tstring filePath) {
 	PEParse::PEParser peclass = PEParse::PEParser();
 	HANDLE peFileMapping = NULL;
 	peFileMapping = peclass.getPEFileMapping(filePath);
+
 }
 
 //PE 파일 경로를 입력받아 DEBUG_FILE_DIRECTORY를 통해 pdb 경로를 구하는 함수
-tstring scanner::Scanner::getPdbPath(const tstring filePath) {
+tstring Scanner::getPdbPath(const tstring filePath) {
 	PEParse::PEParser peclass = PEParse::PEParser();
 	HANDLE peFileMapping = NULL;
 	peFileMapping = peclass.getPEFileMapping(filePath);
+
+	if (peFileMapping != NULL) {
+		IMAGE_DEBUG_DIRECTORY debugDirectory = {0, };
+		DWORD callbackAddress = NULL;
+
+		if ()
+	}
 }
